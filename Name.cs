1b9c74721d9010bb6c12427e73a973c4e95314c1
dagremont@mail.ru <dagremont@mail.ru>
2021-12-19 12:09:02 +0300

    Добавьте файлы проекта.

---
+++ Name.cs
@@ -0,0 +1,6 @@
+﻿namespace ConsoleApp14
+{
+    internal class Name
+    {
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cat cat = new Cat("Кошка");
+            Dog dog = new Dog("Собака");
+            cat.ShowInfo();
+            dog.ShowInfo();
+            Console.ReadKey();
+        }
+        abstract class Animal
+        {
+            public string Name { get; set; }
+            public Animal(string name )
+            {
+                if (name == null)
+                {
+                    Name = "Ворона";
+                }
+                else
+                {
+                    Name = name;
+                }
+            }
+            public abstract void Say();
+            public void ShowInfo()
+            {
+                Console.WriteLine(Name);
+                Say();
+            }
+        }
+        class Dog : Animal
+        {
+            public string song { get; set; }
+            public Dog(string name)
+            : base(name)
+            {
+                song = "Гав";
+            }
+            public override void Say()
+            {
+                Console.WriteLine($"{song}");
+            }
+        }
+        class Cat : Animal
+        {
+            public string song { get; set; }
+            public Cat(string name)
+            : base(name)
+            {
+                song = "Мяу";
+            }
+            public override void Say()
+            {
+                Console.WriteLine($"{song}");
+            }
+        }
+
+    }
+}
